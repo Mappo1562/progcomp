554c2dcc4f354a66fa153561dc6ec5ca36542cf0
Alejandro <alejandro.vergara.araneda@gmail.com>
2025-03-02 17:49:06 -0300

    3_g

--- listado3 - backtracking/G [Paintings]/solucion.cpp
+++ listado3 - backtracking/G [Paintings]/solucion.cpp
@@ -1,27 +1,72 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,m,x;
-    long long sum=0;
-    cin>>n;
-    cin>>m;
-    vector<int> pinturas(n);
-    for(int i=0;i<n;i++)
-        cin>>pinturas[i];
+vector<string> perm;
+vector<bool> usado;
+vector<string> respuesta;
 
-    sort(pinturas.begin(),pinturas.end());
+unordered_map<string,unordered_set<string>> inaceptable;
+vector<string> arr;
+long long sum;
 
-    
-    for (int i=0;i<m;i++){
-        cin>>x;
-        auto it = lower_bound(pinturas.begin(), pinturas.end(), x);
-        if (it != pinturas.end()) {
-            sum += *it - x;
+
+void permute() {
+    if (perm.size() == arr.size()) {
+        if (respuesta.empty()){
+            for (string x:perm)
+                respuesta.push_back(x);
+        }
+        sum++;
+        return;
+    }
+
+    for (unsigned int i = 0; i < arr.size(); i++) {
+        if (!usado[i]) {
+            if (!perm.empty()) {
+                string prev = perm.back();
+                if (inaceptable[prev].count(arr[i])) {
+                    continue;
+                }
+            }
+
+            usado[i] = true;
+            perm.push_back(arr[i]);
+            permute();
+            perm.pop_back();
+            usado[i] = false;
         }
     }
-    cout<<sum<<"\n";
 }
+
+int main(){
+    int n,m,t;
+    cin>>t;
+    for(;t>0;t--){
+        string salida;
+        cin>>n;
+        arr.assign(n, ""); 
+        usado.assign(n, false); 
+        for (int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        string aux1,aux2;
+        cin>>m;
+        for (int i=0;i<m;i++){
+            cin>>aux1>>aux2;
+            inaceptable[aux1].insert(aux2);
+            inaceptable[aux2].insert(aux1);
+        }
+        // funcion q ve las combinaciones
+        permute();
+        cout<<sum<<"\n";
+        for (auto x:respuesta)
+            cout<<x<<" ";
+        cout<<"\n";
+        inaceptable.clear();
+        arr.clear();
+        usado.clear();
+        respuesta.clear();
+        sum=0;
+    }
+
+} 
